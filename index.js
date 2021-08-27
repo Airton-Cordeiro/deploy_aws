80e4a1dce270cd8bd86a35bf371b52158c4cc146
Airton <airton.acds@gmail.com>
2021-08-26 22:16:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express()
+
+app.get("/", (req,res) => {
+    return res.send({message: "servidor online"})
+})
+
+
+app.listen(3333, ()=>{
+    console.log("Server is running on port 3333")
+})
